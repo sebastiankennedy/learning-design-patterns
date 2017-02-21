a8cd678ed7a51e950439a29d045a83af39115a0f
卢艺源 <luyiyuan@anlewo.com>
2017-02-21 15:29:41 +0800

    Singleton

---
+++ Singleton/FirstExample/Preferences.php
@@ -0,0 +1,34 @@
+<?php
+namespace Singleton\FirstExample;
+
+class Preferences
+{
+    private static $instance;
+    private $props = [];
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance() : self
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    public function setProperty(string $key, $val)
+    {
+        $this->props[$key] = $val;
+    }
+
+    public function getProperty(string $key)
+    {
+        if (!array_key_exists($key, $this->props)) {
+            return null;
+        }
+        return $this->props[$key];
+    }
+}
---
+++ Singleton/FirstExample/run.php
@@ -0,0 +1,18 @@
+<?php
+// 注册自动加载
+spl_autoload_register('autoload');
+function autoload($class)
+{
+    require '../../' . str_replace('\\', '/', $class) . '.php';
+}
+
+use Singleton\FirstExample\Preferences;
+
+$preference = Preferences::getInstance();
+$preference->setProperty('name', 'Luis Edware');
+
+unset($preference);
+
+$preference2 = Preferences::getInstance();
+print $preference2->getProperty('name');
+print $preference2->getProperty('someThing');
