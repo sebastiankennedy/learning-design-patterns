da48e7b35412e2ef3b7a93e908dcb76f5f37d1c3
卢艺源 <luyiyuan@anlewo.com>
2017-02-27 22:32:13 +0800

    Facade

--- Facade/FirstExample/Product.php
+++ Facade/FirstExample/Product.php
@@ -1,2 +1,34 @@
 <?php
+namespace Facade\FirstExample;
 
+class Product
+{
+    public $id;
+    public $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
+
+function getProductFileLines($file)
+{
+    return file($file);
+}
+
+function getProductObjectFromId($id, $productName)
+{
+    return new Product($id, $productName);
+}
+
+function getNameFromLine($line)
+{
+    return $line . 'Hello';
+}
+
+function getIdFromLine($line)
+{
+    return $line . 'World';
+}
