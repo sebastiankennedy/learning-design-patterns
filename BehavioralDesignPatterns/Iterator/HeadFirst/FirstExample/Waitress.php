56cf8852321b165ed46a42c5a3659126c9b072df
卢艺源 <luyiyuan@anlewo.com>
2017-03-15 18:28:41 +0800

    Iterator

---
+++ BehavioralDesignPatterns/Iterator/HeadFirst/FirstExample/Waitress.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BehavioralDesignPatterns\Iterator\HeadFirst\FirstExample;
+
+class Waitress
+{
+    public $pancakeHouseMenu;
+    public $dinerMenu;
+
+    public function __construct()
+    {
+
+    }
+
+    public function printMenu()
+    {
+
+    }
+
+    public function printBreakfastMenu()
+    {
+
+    }
+
+    public function printLunchMenu()
+    {
+
+    }
+
+    public function printVegetarianMenu()
+    {
+
+    }
+
+    public function isItemVegetarian($name)
+    {
+
+    }
+}
